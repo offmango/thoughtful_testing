d664daec3dc9945c9fe8dc200531b77b243a31a3
Eric Kurzenberger <offmango@gmail.com>
2012-07-19 14:42:47 -0400

    slight change to spec_helper config

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,10 +2,19 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
-	require 'capybara/rspec'
 	require 'cucumber/rails'
+	require 'capybara/rspec'
 	Capybara.default_selector = :css
 	ActionController::Base.allow_rescue = false
+
+	# Keep if you need to persist in a database; otherwise, delete what follows
+	begin
+	  	DatabaseCleaner.strategy = :transaction
+		rescue NameError
+	  	raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+	end
+	Cucumber::Rails::Database.javascript_strategy = :truncation
+
 end
 
 Spork.each_run do
